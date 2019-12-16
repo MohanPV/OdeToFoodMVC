4bb217ae18edc4e9a1cc60e5ad5305153495617d
MohanPV <mpv159@gmail.com>
2019-12-16 12:42:23 +0530

    Repor COmitted

---
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,51 @@
+﻿using OdeToFood.Data.Models;
+using OdeToFood.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        readonly IRestaurantData db;
+        public RestaurantsController(IRestaurantData db)
+        {
+            this.db = db;
+        }
+
+
+        // GET: Restaurants
+        public ActionResult Index()
+        {
+
+            var model = db.GetAll();
+            return View(model);
+        }
+        public ActionResult Details(int id)
+        {
+            var model = db.Get(id);
+            if (model == null)
+            {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            db.Add(restaurant);
+            return View();
+        }
+    }
+}
