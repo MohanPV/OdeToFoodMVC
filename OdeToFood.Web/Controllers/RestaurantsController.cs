867be2c7ebad98433b4e9fd08297bc41c12218be
MohanPV <mpv159@gmail.com>
2020-01-14 15:42:17 +0530

    COmpleted Module 7

--- OdeToFood.Web/Controllers/RestaurantsController.cs
+++ OdeToFood.Web/Controllers/RestaurantsController.cs
@@ -58,6 +58,7 @@ namespace OdeToFood.Web.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Edit(int id)
         {
             var model = db.Get(id);
@@ -72,16 +73,34 @@ namespace OdeToFood.Web.Controllers
         [HttpPost]
         public ActionResult Edit(Restaurant restaurant)
         {
-            //if (string.IsNullOrEmpty(restaurant.Name))
-            //{
-            //    ModelState.AddModelError(nameof(restaurant.Name), "The Name is required");
-            //}
+           
             if (ModelState.IsValid)
             {
                 db.Update(restaurant);
+                TempData["Message"] = "You have saved the restaurant!...";
                 return RedirectToAction("Details", new { id = restaurant.Id });
             }
             return View(restaurant);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var model = db.Get(id);
+            if (model == null)
+            {
+                return View("NotFound");
+            }
+            return View(model);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection form)
+        {
+
+            db.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 
